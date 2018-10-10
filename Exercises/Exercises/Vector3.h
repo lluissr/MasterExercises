77f6ffdb6fb87fe3138acb3f5e3fe73a60912532
lluissr <lluissr@gmail.com>
2018-10-10 20:07:35 +0200

    Vector3 class is a Template

--- Exercises/Exercises/Vector3.h
+++ Exercises/Exercises/Vector3.h
@@ -1,22 +1,72 @@
+#ifndef __Vector3__
+#define __Vector3__
+#include <math.h>
 
-
+template<class T>
 class Vector3
 {
 private:
-	int x;
-	int y;
-	int z;
+	T x;
+	T y;
+	T z;
 
 public:
 	Vector3();
-	Vector3(int x, int y, int z);
-	Vector3(Vector3 &vector);
+	Vector3(T x, T y, T z);
+	Vector3(const Vector3<T> &vector);
 
 	void Normalize();
-	int Distance_to(Vector3 &vector);
-
-	Vector3& operator+(const Vector3& b);
-	//void operator = (const Vector3 &D);
-
+	T Distance_to(Vector3<T> &vector);
+	Vector3<T> operator+(const Vector3<T>& b);
+	
 	void Print();
 };
+
+
+template <class T>
+Vector3<T>::Vector3() : x(), y(), z() {
+
+}
+
+template <class T>
+Vector3<T>::Vector3(T x, T y, T z) : x(x), y(y), z(z) {
+
+};
+
+template <class T>
+Vector3<T>::Vector3(const Vector3<T>& vector) : x(vector.x), y(vector.y), z(vector.z) {
+
+};
+
+
+template<class T>
+void Vector3<T>::Normalize()
+{
+	T modul = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	x = x / modul;
+	y = y / modul;
+	z = z / modul;
+}
+
+template<class T>
+T Vector3<T>::Distance_to(Vector3<T>& vector)
+{
+	return sqrt(pow(x-vector.x, 2) + pow(y-vector.y, 2) + pow(z-vector.z, 2));
+}
+
+template<class T>
+Vector3<T> Vector3<T>::operator+(const Vector3<T>& b) {
+	Vector3<T> vector;
+	vector.x = this->x + b.x;
+	vector.y = this->y + b.y;
+	vector.z = this->z + b.z;
+	return vector;
+}
+
+template<class T>
+void Vector3<T>::Print()
+{
+	std::cout << "x: " << x << " y: " << y << " z: " << z << "\n";
+}
+
+#endif
